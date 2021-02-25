e116bfaf5a8fd8035fffc48cb80aae83d96c8f33
buketk <61161607+buketk@users.noreply.github.com>
2021-02-25 13:32:23 +0300

    5.ders sonu Inherit, interface

--- OOP3/ApplyManager.cs
+++ OOP3/ApplyManager.cs
@@ -6,10 +6,27 @@ namespace OOP3
 {
     class ApplyManager
     {
-        public void ToApply(ICreditManager creditManager)
+        public void ToApply(ICreditManager creditManager,List<ILoggerService> loggerServices)
         {
             creditManager.Calculate();
+            foreach (var logger in loggerServices)
+            {
+                logger.Log();
+            }
+            
 
         }
+
+
+        public void CreditPreInformation(List<ICreditManager> credits)
+        {
+            foreach (var credit in credits)
+            {
+                credit.Calculate();
+            }
+        }
+
+
     }
+
 }
--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP3
 {
@@ -12,8 +13,18 @@ namespace OOP3
             ICreditManager personalFinanceCreditManager = new PersonalFinanceCreditManager();
 
             ApplyManager applyManager = new ApplyManager();
-            applyManager.ToApply(personalFinanceCreditManager);
-            applyManager.ToApply(vehicleCreditManager);
+            //applyManager.ToApply(personalFinanceCreditManager);
+            //applyManager.ToApply(vehicleCreditManager);
+
+            DatabaseLoggerService databaseLoggerService = new DatabaseLoggerService();
+            FileLoggerService fileLoggerService = new FileLoggerService();
+
+            applyManager.ToApply(personalFinanceCreditManager,new List<ILoggerService> { databaseLoggerService });
+            applyManager.ToApply(vehicleCreditManager,new List<ILoggerService> { databaseLoggerService, fileLoggerService });
+
+            List<ICreditManager> credits = new List<ICreditManager> { vehicleCreditManager , personalFinanceCreditManager };
+            //applyManager.CreditPreInformation(credits);
+
 
         }
     }
